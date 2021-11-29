c5ce99eb3705cf8b612d548119990696c3d770a3
GrowingDev <andreas.benz.privat@icloud.com>
2021-11-30 00:14:11 +0100

    34

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,14 +1,15 @@
 <template>
   <div
-    class="w-auto m-1.5 py-4 flex justify-between items-center sticky top-0 left-0 right-0 inline-block"
+    class="w-auto m-1.5 py-4 flex justify-between items-center sticky top-0 left-0 right-0 inline-block z-10"
   >
     <a>andreas.benz</a>
-    <div class="hidden md:flex justify-between items-center space-x-4">
+    <div class="hidden md:flex justify-between items-center space-x-5 ">
       <a class="link">projects</a>
       <a class="link">skills</a>
       <a class="link">about</a>
       <a class="link">contact</a>
     </div>
+
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class=" container-sm lg:w-10/12 px-1 md:px-8  mx-auto">
     <NavigationBar />
- 
-<div class="container w-full h-full">
-<h1 class="header">We create architecture,
+
+<div class="md:w-7/12  my-16 md:my-32 xl:my-40">
+<h1>We create architecture,
   landscapes, interiors, product design and graphic design.
 </h1>
+<h2>Projects</h2>
 </div>
+
+
 
   </div>
 </template>
@@ -14,3 +17,13 @@
 <script>
 export default {}
 </script>
+
+<style>
+.frame {
+  position:absolute;
+  top: 0;
+  left:0;
+  height:100vh;
+  background:gray;
+}
+</style>
