b6865ff44a723799ee97b1c0ba5f3e259ae0f205
GrowingDev <andreas.benz.privat@icloud.com>
2021-11-29 20:59:18 +0100

    set container

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,12 @@
   <div class=" container-sm lg:w-10/12 px-1 md:px-8  mx-auto">
     <NavigationBar />
  
-
+<div class="container w-full h-full">
 <h1 class="header">We create architecture,
   landscapes, interiors, product design and graphic design.
 </h1>
+</div>
+
   </div>
 </template>
 
