cdf9919d6905f558066139d28a06220f82a32380
GrowingDev <andreas.benz.privat@icloud.com>
2021-11-30 00:38:17 +0100

    er

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class=" container-sm lg:w-10/12 px-1 md:px-8  mx-auto">
-    <NavigationBar />
+  <div>
+    <div class="container-sm h-screen lg:w-10/12 px-1 md:px-8 mx-auto">
+      <NavigationBar />
 
-<div class="md:w-7/12  my-16 md:my-32 xl:my-40">
-<h1>We create architecture,
-  landscapes, interiors, product design and graphic design.
-</h1>
-<h2>Projects</h2>
-</div>
-
-
-
+      <div class="md:w-7/12 my-20 md:my-32 xl:my-40">
+        <h1>
+          We create architecture, landscapes, interiors, product design and
+          graphic design.
+        </h1>
+      </div>
+    </div>
+    <div class="container-sm h-screen lg:w-10/12 px-1 md:px-8 mx-auto">
+      <h2>Projects</h2>
+    </div>
   </div>
 </template>
 
@@ -20,10 +22,10 @@ export default {}
 
 <style>
 .frame {
-  position:absolute;
+  position: absolute;
   top: 0;
-  left:0;
-  height:100vh;
-  background:gray;
+  left: 0;
+  height: 100vh;
+  background: gray;
 }
 </style>
