c8c56330e421153df7e5e7b624780a58a5a64df0
GrowingDev <andreas.benz.privat@icloud.com>
2021-11-30 00:47:47 +0100

    sd

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,7 @@
 <template>
-
   <div>
     <div class="container-sm h-screen lg:w-10/12 px-1 md:px-8 mx-auto">
       <NavigationBar />
-
-  <div class=" container-sm lg:w-10/12 px-1 md:px-8  mx-auto">
-    <NavigationBar />
-
-<div class="container lg:w-8/12 h-full">
-<h1>We create architecture,
-  landscapes, interiors, product design and graphic design.
-</h1>
-<h2>Projects</h2>
-</div>
 
       <div class="md:w-7/12 my-20 md:my-32 xl:my-40">
         <h1>
