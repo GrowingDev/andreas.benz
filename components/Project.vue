463f927a58eb34ef8b038310e2791b6e5dfd0d70
GrowingDev <andreas.benz.privat@icloud.com>
2021-11-30 13:51:30 +0100

    resize

--- components/Project.vue
+++ components/Project.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full md:w-5/12 xl:w-4/12 md:ml-4 my-2 flex flex-col">
+  <div class="w-full md:w-5/12 xl:w-4/12 md:ml-4 my-3 flex flex-col">
     <div class="w-full bg-gray-300">
       <img src="https://picsum.photos/400/400" class="h-60 lg:h-60 w-full" />
     </div>
@@ -7,6 +7,6 @@
       Our working method practices a simultaneous exploration of traditional
       handicraft and cutting edge digital technology
     </p>
-    <a class="link w-max" href="https://google.com">read more &#8594;</a>
+    <a class="link w-max " href="https://google.com">read more &#8594;</a>
   </div>
 </template>
