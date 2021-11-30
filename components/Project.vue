2f06ad287c7944f0e605f2abebed3ca3ed43f2c9
GrowingDev <andreas.benz.privat@icloud.com>
2021-11-30 13:25:26 +0100

    refac

--- components/Project.vue
+++ components/Project.vue
@@ -3,7 +3,7 @@
         <div class="w-full bg-gray-300">
           <img src="https://picsum.photos/400/400" class="h-60 lg:h-&0 w-full" />
         </div>
-        <p class="text-base py-2">Our working method practices a simultaneous exploration of traditional handicraft and cutting edge digital technology</p>
-        <a class="link w-max">read more</a>
+        <p class="text-base py-1">Our working method practices a simultaneous exploration of traditional handicraft and cutting edge digital technology</p>
+        <a class="link w-max ">read more</a>
       </div>
 </template>
