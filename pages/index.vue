3569ca33e3e0043a05c4cb0d1de6180e5abea091
GrowingDev <andreas.benz.privat@icloud.com>
2021-11-30 13:16:09 +0100

    reafactoring

--- pages/index.vue
+++ pages/index.vue
@@ -12,28 +12,10 @@
     </div>
     <Divider title="Projects" />
     <div class="w-full flex flex-row flex-wrap justify-end py-3">
-      <div class="w-full md:w-5/12 xl:w-4/12 md:ml-4 my-2 flex flex-col">
-        <div class="w-full bg-gray-300">
-          <img src="https://picsum.photos/400/400" class="h-60 lg:h-&0 w-full" />
-        </div>
-        <p class="text-base">Lorem ipsum dolorem ese de lari</p>
-        <a class="link w-max">read more</a>
-      </div>
-
-        <div class="w-full md:w-5/12 xl:w-4/12 md:ml-4 my-2 flex flex-col">
-        <div class="w-full bg-gray-300">
-          <img src="https://picsum.photos/400/400" class="h-60 lg:h-&0 w-full" />
-        </div>
-        <p class="text-base">Lorem ipsum dolorem ese de lari</p>
-        <a class="link w-max">read more</a>
-      </div>
-        <div class="w-full md:w-5/12 xl:w-4/12 md:ml-4 my-2 flex flex-col">
-        <div class="w-full bg-gray-300">
-          <img src="https://picsum.photos/400/400" class="h-60 lg:h-&0 w-full" />
-        </div>
-        <p class="text-base">Lorem ipsum dolorem ese de lari</p>
-        <a class="link w-max">read more</a>
-      </div>
+      <Project />
+      <Project />
+      <Project />
+      <Project />
     </div>
     <Divider title="Skills" />
   </div>
