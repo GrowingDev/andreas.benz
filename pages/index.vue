81fdc87d85496d61b99c6449e76c256988748854
GrowingDev <andreas.benz.privat@icloud.com>
2021-11-30 10:42:51 +0100

    bugfix

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,23 @@
 <template>
-
-  <div class=" px-3 w-full md:w-11/12 lg:w-10/12 xl:w-8/12 md:mx-auto flex flex-col justify-start">
+  <div
+    class="px-3 w-full md:w-11/12 lg:w-10/12 xl:w-8/12 md:mx-auto flex flex-col justify-start"
+  >
     <NavigationBar />
- 
-<div class="container h-screen w-full flex flex-col justify-center">
-<h1 >We create architecture,
-  landscapes, interiors, product design and graphic design.
-</h1>
 
-</div>
-<Divider title="Projects"/>
-<div class="container h-screen w-full flex flex-col justify-center">
-<h1 >We create architecture,
-  landscapes, interiors, product design and graphic design.
-</h1>
-
-</div>
-<Divider title="Skills"/>
-
-
-  <div>
-    <div class="container-sm h-screen lg:w-10/12 px-1 md:px-8 mx-auto">
-      <NavigationBar />
-
-      <div class="md:w-7/12 my-20 md:my-32 xl:my-40">
-        <h1>
-          We create architecture, landscapes, interiors, product design and
-          graphic design.
-        </h1>
-      </div>
+    <div class="container h-screen w-full flex flex-col justify-center">
+      <h1>
+        We create architecture, landscapes, interiors, product design and
+        graphic design.
+      </h1>
     </div>
-    <div class="container-sm h-screen lg:w-10/12 px-1 md:px-8 mx-auto">
-      <h2>Projects</h2>
+    <Divider title="Projects" />
+    <div class="container h-screen w-full flex flex-col justify-center">
+      <h1>
+        We create architecture, landscapes, interiors, product design and
+        graphic design.
+      </h1>
     </div>
+    <Divider title="Skills" />
   </div>
 </template>
 
@@ -40,6 +25,4 @@
 export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
