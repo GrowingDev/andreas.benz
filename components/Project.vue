c0ac818b5ef9c1c456dfe7c29a9d7e3e9fc40c63
GrowingDev <andreas.benz.privat@icloud.com>
2021-11-30 13:38:13 +0100

    test

--- components/Project.vue
+++ components/Project.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="w-full md:w-5/12 xl:w-4/12 md:ml-4 my-2 flex flex-col">
-        <div class="w-full bg-gray-300">
-          <img src="https://picsum.photos/400/400" class="h-60 lg:h-&0 w-full" />
-        </div>
-        <p class="text-base py-1">Our working method practices a simultaneous exploration of traditional handicraft and cutting edge digital technology</p>
-        <a class="link w-max ">read more 	&#8594;</a>
-      </div>
+  <div class="w-full md:w-5/12 xl:w-4/12 md:ml-4 my-2 flex flex-col">
+    <div class="w-full bg-gray-300">
+      <img src="https://picsum.photos/400/400" class="h-60 lg:h-60 w-full" />
+    </div>
+    <p class="text-base py-1">
+      Our working method practices a simultaneous exploration of traditional
+      handicraft and cutting edge digital technology
+    </p>
+    <a class="link w-max" href="https://google.com">read more &#8594;</a>
+  </div>
 </template>
