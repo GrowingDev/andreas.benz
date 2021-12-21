911c1fe0ce25d02f77cdba0f2fc4b6d39ecbb82f
GrowingDev <andreas.benz.privat@icloud.com>
2021-12-21 22:36:42 +0100

    fg

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,14 +22,14 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "~/assets/css/main.scss",
-    '@fortawesome/fontawesome-svg-core/styles.css'
+
   ],
   purgeCSS: {
   whitelistPatterns: [/svg.*/, /fa.*/]
 },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/fontawesome.js'
+
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
